021459ba905172f1638a8e3731cac20ed206851a
Evgeny Kuzin <ekuzin@outlook.com>
2021-10-06 14:48:53 +0700

    update Differ.php

--- src/Differ.php
+++ src/Differ.php
@@ -1,36 +1,45 @@
 <?php
 
-function parse(string $string)
+namespace Differ\Differ;
+
+function genDiff(string $firstPath, string $secondPath) : string
 {
-    $array = json_decode($string, true);
-    return $array;
+    $arr_1 = (parse($firstPath));
+    $arr_2 = (parse($secondPath));
+    $arr_3 = array_merge($arr_1, $arr_2);
+    $arr_4 = array_keys($arr_3);
+    sort($arr_4);
+    return compare($arr_4, $arr_1, $arr_2);
 }
 
 
-$example_1 =
-'{
-  "host": "hexlet.io",
-  "timeout": 50,
-  "proxy": "123.234.53.22",
-  "follow": false
-}';
 
-$example_2 =
-'{
-  "timeout": 20,
-  "verbose": true,
-  "host": "hexlet.io"
-}';
+function compare(array $commonKeys, array $first, array $second) : string
+{
+    $first = boolAsString($first);
+    $second = boolAsString($second);
+    $result = '';
+    foreach ($commonKeys as $key) {
+        if (array_key_exists($key, $first) && array_key_exists($key, $second) && $first[$key] === $second[$key]) {
+            $result = $result . "  " . $key . ": " . $first[$key] . "\n";
+        } elseif (array_key_exists($key, $first) && array_key_exists($key, $second) && $first[$key] !== $second[$key]) {
+            $result = $result . " -" . $key . ": " . $first[$key] . "\n" . " +" . $key . ": " . $second[$key] . "\n";
+        } elseif (array_key_exists($key, $first) && !array_key_exists($key, $second)) {
+            $result = $result . " -" . $key . ": " . $first[$key] . "\n";
+        } elseif (!array_key_exists($key, $first) && array_key_exists($key, $second)) {
+            $result = $result . " +" . $key . ": " . $second[$key] . "\n";
+        }
+    }
+    return "{" . "\n" . $result . "}" . "\n";
+}
 
+function parse(string $path) : array
+{
+    $fileContent = file_get_contents($path);
+    return json_decode($fileContent, true);
+}
 
-$arr_1 = (parse($example_1));
-$arr_2 = (parse($example_2));
-ksort($arr_1);
-ksort($arr_2);
-var_dump($arr_1);
-var_dump($arr_2);
-
-function boolToString(array $array)
+function boolAsString(array $array) : array
 {
     return array_map(function ($value) {
         if ($value === true) {
@@ -41,60 +50,4 @@ function boolToString(array $array)
         return $value;
     }, $array);
 }
-
-function getSameElements(array $array_1, array $array_2)
-{
-    $same = array_intersect($array_1, $array_2);
-    $item = [];
-    foreach ($same as $key => $value) {
-        $item = [$key => $value];
-    }
-    return $item;
-}
-
-function getOnlyFirstElements(array $arr1, array $arr2)
-{
-    $onlyFirst = array_diff_key($arr1, $arr2);
-    //$onlyFirst = boolToString($onlyFirst);
-    $item = [];
-    foreach($onlyFirst as $key => $value) {
-        $item[$key] = $value;
-    }
-    return $item;
-}
-
-function getOnlySecondElements(array $arr1, array $arr2)
-{
-    $onlySecond = array_diff_key($arr2, $arr1);
-    //$onlySecond = boolToString($onlySecond);
-    $item = [];
-    foreach($onlySecond as $key => $value) {
-        $item[$key] = $value;
-    }
-    return $item;
-}
-
-function getCommonElements(array $arr1, array  $arr2)
-{
-    $item = '';
-    foreach ($arr1 as $key => $value) {
-        if (array_key_exists($key, $arr2) && $arr1[$key] !== $arr2[$key]) {
-            $item = $item . "- " . $key . ": " . $value . "\n" . "+ " . $key . ": " . $arr2[$key] . "\n";
-        }
-    }
-    return $item;
-}
-$common = getCommonElements($arr_1, $arr_2);
-var_dump($common);
-$same = getSameElements($arr_1, $arr_2);
-var_dump($same);
-$onlyFirst = getOnlyFirstElements($arr_1, $arr_2);
-var_dump($onlyFirst);
-$onlySecond = getOnlySecondElements($arr_1, $arr_2);
-var_dump($onlySecond);
-
-//$result = $same . $onlyFirst . $onlySecond . $common;
-//var_dump($result);
-
-
 
