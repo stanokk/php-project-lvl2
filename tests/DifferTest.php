290c1b9771d9d3d943a26a2e888db5e0da9b86e5
Evgeny Kuzin <ekuzin@outlook.com>
2021-10-11 15:16:45 +0700

    update DifferTest.php

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -1,1 +1,19 @@
 <?php
+
+namespace Differ\Phpunit\Differ;
+
+use PHPUnit\Framework\TestCase;
+
+use function Differ\Differ\genDiff;
+
+class DifferTest extends TestCase
+{
+    public function testDiff(): void
+    {
+        $expected = file_get_contents(__DIR__ . "/fixtures/expected.txt");
+        $first = __DIR__ . "/fixtures/first.json";
+        $second = __DIR__ . "/fixtures/second.json";
+        $result = genDiff($first, $second);
+        $this->assertEquals($expected, $result);
+    }
+}
