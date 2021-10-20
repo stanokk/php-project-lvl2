5e52e5263fc9d683e5abb5cd311976e46f2c7147
Evgeny Kuzin <ekuzin@outlook.com>
2021-10-20 13:02:19 +0700

    update DifferTest.php

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -10,15 +10,15 @@ class DifferTest extends TestCase
 {
     public function testDiff(): void
     {
-        $expected = file_get_contents(__DIR__ . "/fixtures/expected.txt");
+        //$expected = file_get_contents(__DIR__ . "/fixtures/expected.txt");
         $expectedStylish = file_get_contents(__DIR__ . "/fixtures/expectedStylish.txt");
         $expectedPlain = file_get_contents(__DIR__ . "/fixtures/expectedPlain.txt");
         $expectedJson = file_get_contents(__DIR__ . "/fixtures/expectedJson.txt");
 
-        $firstJson = __DIR__ . "/fixtures/first.json";
-        $secondJson = __DIR__ . "/fixtures/second.json";
-        $result = genDiff($firstJson, $secondJson);
-        $this->assertEquals($expected, $result);
+        //$firstJson = __DIR__ . "/fixtures/first.json";
+        //$secondJson = __DIR__ . "/fixtures/second.json";
+        //$result = genDiff($firstJson, $secondJson);
+        //$this->assertEquals($expected, $result);
 
         $firstYml = __DIR__ . "/fixtures/first.yml";
         $secondYml = __DIR__ . "/fixtures/second.yml";
